ac90957ffb06e47b042de74aba822e045cf2c118
Necati Uğur KARAKARA <necatiugurkara@gmail.com>
2026-04-18 19:39:59 +0300

    Proje oluşturuldu, temel klasör yapısı eklendi ve Firebase paketleri kuruldu

---
+++ README.md
@@ -0,0 +1,3 @@
+# project_final_halisaha
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/project_final_halisaha/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.project_final_halisaha
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
